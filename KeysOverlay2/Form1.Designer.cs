090164b09c257a1ae504c4743779cbb2073fecf7
DoshiPanda <69212711+Softiamge@users.noreply.github.com>
2023-08-10 19:33:10 +0300

    code_upload

---
+++ KeysOverlay2/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace KeysOverlay2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel20 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.panel18 = new System.Windows.Forms.Panel();
+            this.panel19 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.p2 = new System.Windows.Forms.Panel();
+            this.p4 = new System.Windows.Forms.Panel();
+            this.p3 = new System.Windows.Forms.Panel();
+            this.p1 = new System.Windows.Forms.Panel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifycontextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.notifycontextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.panel20, 4, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel13, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel12, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel11, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel14, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel15, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel17, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel16, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel18, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel19, 3, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(319, 383);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(195, 79);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // panel20
+            // 
+            this.panel20.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel20.Location = new System.Drawing.Point(159, 42);
+            this.panel20.Name = "panel20";
+            this.panel20.Size = new System.Drawing.Size(32, 32);
+            this.panel20.TabIndex = 11;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel13.Location = new System.Drawing.Point(81, 3);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(32, 32);
+            this.panel13.TabIndex = 3;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel12.Location = new System.Drawing.Point(42, 3);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(32, 32);
+            this.panel12.TabIndex = 2;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel11.Location = new System.Drawing.Point(3, 3);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(32, 32);
+            this.panel11.TabIndex = 1;
+            // 
+            // panel14
+            // 
+            this.panel14.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel14.Location = new System.Drawing.Point(120, 3);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(32, 32);
+            this.panel14.TabIndex = 10;
+            // 
+            // panel15
+            // 
+            this.panel15.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel15.Location = new System.Drawing.Point(159, 3);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(32, 32);
+            this.panel15.TabIndex = 5;
+            // 
+            // panel17
+            // 
+            this.panel17.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel17.Location = new System.Drawing.Point(42, 42);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(32, 32);
+            this.panel17.TabIndex = 7;
+            // 
+            // panel16
+            // 
+            this.panel16.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel16.Location = new System.Drawing.Point(3, 42);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(32, 32);
+            this.panel16.TabIndex = 6;
+            // 
+            // panel18
+            // 
+            this.panel18.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel18.Location = new System.Drawing.Point(81, 42);
+            this.panel18.Name = "panel18";
+            this.panel18.Size = new System.Drawing.Size(32, 32);
+            this.panel18.TabIndex = 8;
+            // 
+            // panel19
+            // 
+            this.panel19.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel19.Location = new System.Drawing.Point(120, 42);
+            this.panel19.Name = "panel19";
+            this.panel19.Size = new System.Drawing.Size(32, 32);
+            this.panel19.TabIndex = 9;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.p2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.p4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.p3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.p1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(196, 383);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(117, 79);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.p2.Location = new System.Drawing.Point(42, 3);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(32, 32);
+            this.p2.TabIndex = 2;
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.DodgerBlue;
+            this.p4.Location = new System.Drawing.Point(81, 42);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(32, 32);
+            this.p4.TabIndex = 6;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.p3.Location = new System.Drawing.Point(42, 42);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(32, 32);
+            this.p3.TabIndex = 8;
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.p1.Location = new System.Drawing.Point(3, 42);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(32, 32);
+            this.p1.TabIndex = 4;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.notifycontextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "KeysOverlay";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifycontextMenuStrip1
+            // 
+            this.notifycontextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.notifycontextMenuStrip1.Name = "notifycontextMenuStrip1";
+            this.notifycontextMenuStrip1.Size = new System.Drawing.Size(104, 70);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 474);
+            this.ContextMenuStrip = this.notifycontextMenuStrip1;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KeysOverlay.v2";
+            this.TransparencyKey = System.Drawing.Color.Magenta;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.notifycontextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel p3;
+        private System.Windows.Forms.Panel p4;
+        private System.Windows.Forms.Panel p1;
+        private System.Windows.Forms.Panel p2;
+        private System.Windows.Forms.Panel panel20;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip notifycontextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KeysOverlay2/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace KeysOverlay2
+{
+    public partial class Form1 : Form
+    {
+               
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        GlobalKeyboardHook gkh = new GlobalKeyboardHook();
+        private PanelList lst = new PanelList();
+
+
+
+
+
+        //public LinkedList<Panel_link> buttonmap = new LinkedList<Panel_link>();
+        private Keys[] default_keys = 
+        {   
+            Keys.W, Keys.A, Keys.S, Keys.D, 
+            Keys.Y, Keys.H, Keys.U, Keys.J, Keys.I, 
+            Keys.K, Keys.O, Keys.L, Keys.V, 
+            Keys.B, Keys.N,
+            Keys.NumPad4,Keys.NumPad5,Keys.NumPad6,
+            Keys.NumPad7,Keys.NumPad8,Keys.NumPad9,
+            Keys.Add, Keys.Subtract 
+        };
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Panel_link blink = new Panel_link();
+            //link(panel1, Keys.W);
+            label1.Visible = false;
+            
+            notifyIcon1.Visible = true;
+
+            object zz = Controls;
+            
+            gkh.HookedKeys.AddRange(default_keys);
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyU);
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyD);
+
+            
+            
+        }
+
+        private void gkh_KeyU(object sender, KeyEventArgs e)
+        {
+            //label1.Text = "Отпустили " + e.KeyCode.ToString();
+            int state = 0;
+            Panels_switch(e.KeyData, state);
+            label1.Text = e.KeyData.ToString();
+
+
+        }
+        
+        private void Panels_switch(Keys key, int state)
+        {
+            switch (key)
+            {
+                case Keys.A:
+                    PanelColorSwitch(4, state);
+                    break;
+                case Keys.W:
+                    PanelColorSwitch(2, state);
+                    break;
+                case Keys.S:
+                    PanelColorSwitch(8, state);
+                    break;
+                case Keys.D:
+                    PanelColorSwitch(6, state);
+                    break;
+                case Keys.Y:
+                    PanelColorSwitch(12, state);
+                    break;
+                case Keys.Add:
+                    PanelColorSwitch(12, state);
+                    break;
+                case Keys.U:
+                    PanelColorSwitch(13, state);
+                    break;
+                case Keys.NumPad7:
+                    PanelColorSwitch(13, state);
+                    break;
+                case Keys.I:
+                    PanelColorSwitch(14, state);
+                    break;
+                case Keys.NumPad8:
+                    PanelColorSwitch(14, state);
+                    break;
+                case Keys.O:
+                    PanelColorSwitch(15, state);
+                    break;
+                case Keys.NumPad9:
+                    PanelColorSwitch(15, state);
+                    break;
+                case Keys.H:
+                    PanelColorSwitch(17, state);
+                    break;
+                case Keys.Subtract:
+                    PanelColorSwitch(17, state);
+                    break;
+                case Keys.J:
+                    PanelColorSwitch(18, state);
+                    break;
+                case Keys.NumPad4:
+                    PanelColorSwitch(18, state);
+                    break;
+                case Keys.K:
+                    PanelColorSwitch(19, state);
+                    break;
+                case Keys.NumPad5:
+                    PanelColorSwitch(19, state);
+                    break;
+                case Keys.L:
+                    PanelColorSwitch(20, state);
+                    break;
+                case Keys.NumPad6:
+                    PanelColorSwitch(20, state);
+                    break;
+            }
+        }
+
+        private void gkh_KeyD(object sender, KeyEventArgs e)
+        {
+            int state = 1;
+            //label1.Text = "Нажали " + e.KeyCode.ToString();
+            Panels_switch(e.KeyData, state);
+        }
+
+
+        public void PanelColorSwitch(int panel, int state)
+        {
+            Color cl1 = Color.DodgerBlue, cl2 = Color.Black;
+            switch (panel)
+            {
+                case 2:
+                    if (state == 0) { p2.BackColor = cl1; }
+                    else { p2.BackColor = cl2; }
+                    break;
+                
+                case 4:
+                    if (state == 0) { p1.BackColor = cl1; }
+                    else { p1.BackColor = cl2; }
+                    break;
+                
+                case 6:
+                    if (state == 0) { p4.BackColor = cl1; }
+                    else { p4.BackColor = cl2; }
+                    break;
+                
+                case 8:
+                    if (state == 0) { p3.BackColor = cl1; }
+                    else { p3.BackColor = cl2; }
+                    break;
+                
+                case 11:
+                    if (state == 0) { panel11.BackColor = cl1; }
+                    else { panel11.BackColor = cl2; }
+                    break;
+                case 12:
+                    if (state == 0) { panel12.BackColor = cl1; }
+                    else { panel12.BackColor = cl2; }
+                    break;
+                case 13:
+                    if (state == 0) { panel13.BackColor = cl1; }
+                    else { panel13.BackColor = cl2; }
+                    break;
+                case 14:
+                    if (state == 0) { panel14.BackColor = cl1; }
+                    else { panel14.BackColor = cl2; }
+                    break;
+                case 15:
+                    if (state == 0) { panel15.BackColor = cl1; }
+                    else { panel15.BackColor = cl2; }
+                    break;
+                case 16:
+                    if (state == 0) { panel16.BackColor = cl1; }
+                    else { panel16.BackColor = cl2; }
+                    break;
+                case 17:
+                    if (state == 0) { panel17.BackColor = cl1; }
+                    else { panel17.BackColor = cl2; }
+                    break;
+                case 18:
+                    if (state == 0) { panel18.BackColor = cl1; }
+                    else { panel18.BackColor = cl2; }
+                    break;
+                case 19:
+                    if (state == 0) { panel19.BackColor = cl1; }
+                    else { panel19.BackColor = cl2; }
+                    break;
+                case 20:
+                    if (state == 0) { panel20.BackColor = cl1; }
+                    else { panel20.BackColor = cl2; }
+                    break;
+
+            }
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Magenta;
+            tableLayoutPanel1.BackColor = Color.Magenta;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.WhiteSmoke;
+            tableLayoutPanel1.BackColor = Color.WhiteSmoke;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.TopMost = false;
+            
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            label1.Text = e.KeyData.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false; 
+        }
+    }
+
+
+    class PanelList
+    {
+        List<Panel_link> pan_lnk = new List<Panel_link>();
+        class Panel_link
+        {
+            public Panel pan;
+            public Keys button;
+            public Panel_link(Panel aa, Keys bb)
+            {
+                pan = aa;
+                button = bb;
+            }
+
+            public void Clear()
+            {
+                pan = null;
+                button = Keys.None;
+
+            }
+
+
+        }
+
+
+        public void Add(Panel a, Keys b)
+        {
+            Panel_link zzz = new Panel_link(a, b);
+            pan_lnk.Add(zzz);
+        }
+
+        public Object Find(Panel a)
+        {
+            return pan_lnk.Find(ff => ff.pan == a);
+        }
+        public Object FindAll(Panel a)
+        {
+            return pan_lnk.FindAll(ff => ff.pan == a);
+        }
+
+
+        public int FindIndex(Panel a)
+        {
+            return pan_lnk.FindIndex(ff => ff.pan == a);
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
